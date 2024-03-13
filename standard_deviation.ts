360a6e26e5f28e17109eb4095d4ffc9cd5be0083
Mateus Queirós <mateuscqueiros@gmail.com>
2024-03-12 22:31:07 -0300

    Initial

---
+++ standard_deviation.ts
@@ -0,0 +1,60 @@
+function getStandardDeviation(array: number[]) {
+  const n = array.length
+  const mean = getMean(array)
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
+
+function getStandardDeviationFrom3(array: number[]) {
+  const n = array.length
+  const newArray = [Math.min(...array), Math.max(...array)]
+  const mean = getMean(newArray)
+  return Math.sqrt(newArray.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
+
+function getMean(data: number[]) {
+  const sum = data.reduce((acc, item) => acc + item, 0)
+  return sum / data.length
+}
+
+function getZScore(score: number, data: number[]) {
+  const mean = getMean(data);
+  const standardDeviation = getStandardDeviation(data);
+
+  return (score - mean) / standardDeviation
+}
+
+const data = [100, 101.25, 102, 99, 98.25, 100, 100.50, 99.75, 97.25]
+const data3 = [98, 98.25, 98.75, 99, 99.25, 99.5, 100, 100.5, 100.75, 101, 101.25, 101.5, 102, 103]
+
+const scoreInput = document.querySelector("#score") as HTMLInputElement
+const dataInput = document.querySelector("#data") as HTMLInputElement
+const submitButton = document.querySelector("#submit")
+const formattedData = document.querySelector("#formatted-data")
+dataInput.value = data3.toString()
+scoreInput.value = Number(104).toString()
+
+submitButton?.addEventListener("click", () => {
+  const score = Number(scoreInput.value)
+  const allData = dataInput.value.split(",").map((item) => Number(item))
+  const data = allData
+
+  const zScore = getZScore(score, data)
+  const standardDeviation = getStandardDeviationFrom3(data)
+  const mean = getMean(data)
+
+  console.log("Last:", score)
+  console.log("Z Score:", zScore)
+  console.log("Standard Deviation:", standardDeviation)
+  console.log("Mean:", mean)
+  console.log("Min/Max:", Math.min(...data), "/", Math.max(...data))
+  console.log("=========")
+
+  if (formattedData) {
+    formattedData.innerHTML = ""
+  }
+  data.forEach(item => {
+    const span = document.createElement("span")
+    span.innerHTML = String(item)
+    formattedData?.appendChild(span)
+  })
+})
